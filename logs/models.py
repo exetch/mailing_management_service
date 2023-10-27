be9f4ed0b83f054bab6afb896499ed97e7f6a673
exetch <gassemark@gmail.com>
2023-10-27 18:04:13 +0300

    finalization_2 of the mailing script

--- logs/models.py
+++ logs/models.py
@@ -9,7 +9,6 @@ class MailingLog(models.Model):
     sent_datetime = models.DateTimeField(auto_now=True, verbose_name="Дата и время последней попытки отправки")
     status = models.CharField(max_length=20)
     server_response = models.TextField(**NULLABLE)
-    error_type = models.CharField(max_length=50, **NULLABLE)
 
     def __str__(self):
         return f"Лог {self.id}"
